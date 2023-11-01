1743c2129c13182f179bceced6b24dc54e9aa5c5
parimal123 <parimal.d.deshmukh>
2023-11-01 22:00:23 +0530

    Intial commit

---
+++ src/main/java/com/reliance/reliancemart/RelianceMartApplication.java
@@ -0,0 +1,13 @@
+package com.reliance.reliancemart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelianceMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelianceMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reliance/reliancemart/RelianceMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reliance.reliancemart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelianceMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
